9d09e0d0c1cfcbf99d6bfa69aa7fa1e268c37a7c
chewbecca <beckybettinger@yahoo.de>
2019-12-24 00:39:46 +0100

    create Assets

--- src/colorCalculations.js
+++ src/colorCalculations.js
@@ -1,47 +1,66 @@
-function generateSteps(colorValue) {
-    let baseRelLuminance = Math.round(calcRelativeLuminance(colorValue) * 10);
-    console.log(baseRelLuminance);
-    return baseRelLuminance;
+export function generateSteps(colorValue) {
+  let baseRelLuminance = Math.round(calcRelativeLuminance(colorValue) * 10);
+  console.log(baseRelLuminance);
+  return baseRelLuminance;
 }
-
 
 export function calcShade(colorValue, factor, step) {
+  let newRGB = {};
 
-    let newRGB = {};
+  newRGB.r = Math.floor((colorValue.r / factor) * step);
+  newRGB.g = Math.floor((colorValue.g / factor) * step);
+  newRGB.b = Math.floor((colorValue.b / factor) * step);
 
-    newRGB.r = Math.floor(colorValue.r / factor*step);
-    newRGB.g = Math.floor(colorValue.g / factor*step);
-    newRGB.b = Math.floor(colorValue.b / factor*step);
-
-    return newRGB;
+  return newRGB;
 }
 
-export function calcTint(colorValue, factor) {
+export function calcTint(colorValue, factor, step) {
+  let newRGB = {};
 
-    let newRGB = {};
-
-    newRGB.r = Math.floor(((255 - colorValue.r) * factor) / 10 + colorValue.r);
-    newRGB.g = Math.floor(((255 - colorValue.g) * factor) / 10 + colorValue.g);
-    newRGB.b = Math.floor(((255 - colorValue.b) * factor) / 10 + colorValue.b);
-    return "rgb(" + newRGB[0] + ", " + newRGB[1] + ", " + newRGB[2] + ")";
+  newRGB.r = Math.floor(((255 - colorValue.r) * factor) / step + colorValue.r);
+  newRGB.g = Math.floor(((255 - colorValue.g) * factor) / step + colorValue.g);
+  newRGB.b = Math.floor(((255 - colorValue.b) * factor) / step + colorValue.b);
+  return newRGB;
 }
 
 function calcRelativeLuminance(colorValue) {
-    colorValue.r = normalizeValue(colorValue.r) * 0.2126;
-    colorValue.g = normalizeValue(colorValue.g) * 0.7152;
-    colorValue.b = normalizeValue(colorValue.b) * 0.0722;
+  colorValue.r = normalizeValue(colorValue.r) * 0.2126;
+  colorValue.g = normalizeValue(colorValue.g) * 0.7152;
+  colorValue.b = normalizeValue(colorValue.b) * 0.0722;
 
-    return colorValue.r + colorValue.g + colorValue.b;
+  return colorValue.r + colorValue.g + colorValue.b;
 }
 
 function normalizeValue(singleColorValue) {
-    singleColorValue = singleColorValue / 255;
-    if (singleColorValue <= 0.03928) {
-        console.log("value is darker")
-        singleColorValue = 0.03928 / 12.92;
-    } else {
-        console.log("value is brighter")
-        singleColorValue = ((singleColorValue + 0.055) / 1.055) ** 2.4;
-    }
-    return singleColorValue;
+  singleColorValue = singleColorValue / 255;
+  if (singleColorValue <= 0.03928) {
+    singleColorValue = 0.03928 / 12.92;
+  } else {
+    singleColorValue = ((singleColorValue + 0.055) / 1.055) ** 2.4;
+  }
+  return singleColorValue;
+}
+
+export function colorObjectToRGBString(colorObject) {
+  return (
+    "rgb(" + colorObject.r + ", " + colorObject.g + ", " + colorObject.b + ")"
+  );
+}
+
+export function sketchHexToColorObject(sketchHex) {
+  let colorObject = {};
+
+  colorObject.r = parseInt(sketchHex.slice(1,2), "16");
+  colorObject.g = parseInt(sketchHex.slice(3,4), "16");
+  colorObject.b = parseInt(sketchHex.slice(5,6), "16");
+
+}
+
+export function colorObjectToSketchHex(colorObject) {
+  return (
+    "#" +
+    colorObject.r.toString(16) +
+    colorObject.r.toString(16) +
+    colorObject.b.toString(16)
+  );
 }
--- src/createSketchAssets.js
+++ src/createSketchAssets.js
@@ -1,90 +1,77 @@
-function generateSteps(colorValue) {
-    var baseRelLuminance = Math.round(calcRelativeLuminance(colorValue) * 10);
-    console.log(baseRelLuminance);
-    return baseRelLuminance;
+import * as colorCalc from "./colorCalculations";
+import sketch from "sketch";
+import { colorObjectToRGBString } from "./colorCalculations";
+import { calcTint } from "./colorCalculations";
+
+function createDocumentColors(selectedColor, document, title) {
+  let steps = colorCalc.generateSteps(selectedColor);
+  let colorGroup = new sketch.Group();
+
+  for (let j = 10; j > steps; j--) {
+    colorGroup.layers.push(
+      new sketch.Shape({
+        name: title + " " + j,
+        style: {
+          fills: [
+            {
+              color: colorCalc.colorObjectToRGBString(
+                colorCalc.calcTint(selectedColor, steps, j)
+              )
+            }
+          ]
+        }
+      })
+    );
+  }
+
+  colorGroup.layers.push(
+    new sketch.Shape({
+      name: title + " Primary",
+      style: {
+        fills: [
+          {
+            color: selectedColor
+          }
+        ]
+      }
+    })
+  );
+
+  for (let i = steps; i >= 0; i--) {
+    colorGroup.layers.push(
+      new sketch.Shape({
+        name: title + " " + j,
+        style: {
+          fills: [
+            {
+              color: colorCalc.colorObjectToRGBString(
+                colorCalc.calcShades(selectedColor, steps, j)
+              )
+            }
+          ]
+        }
+      })
+    );
+  }
+
+  addToDocumentColors(colorGroup, document);
+  addToSharedStyles(colorGroup,document);
 }
 
-function initializeColors(baseColor) {
-    colorBox.innerHTML = "";
-    var colorField = document.createElement("div");
-    colorField.className = "colorField";
-    colorField.style.backgroundColor = baseColor;
-    colorField.innerHTML = "base";
-
-    var steps = generateSteps(colorField.style.backgroundColor);
-
-    for (var j = 10; j > steps; j--) {
-        var tint = document.createElement("div");
-        tint.style.backgroundColor = calcTint(colorField.style.backgroundColor, j);
-        tint.innerHTML = calcRelativeLuminance(tint.style.backgroundColor).toFixed(
-            2
-        );
-        tint.className = "colorField";
-        colorBox.appendChild(tint);
-    }
-
-    colorBox.appendChild(colorField);
-
-    for (var i = steps; i >= 0; i-- ){
-        var shade = document.createElement("div");
-        shade.style.backgroundColor = calcShade(
-            colorField.style.backgroundColor,
-            steps, i
-        );
-        shade.innerHTML = calcRelativeLuminance(
-            shade.style.backgroundColor
-        ).toFixed(2);
-        shade.className = "colorField";
-        colorBox.appendChild(shade);
-    }
+function addToSharedStyles(group, document) {
+  group.layers.map(layer => {
+    document.sharedLayerStyles.push({
+      name: layer.name,
+      style: layer.style
+    });
+  });
 }
 
-function calcShade(colorValue, factor, step) {
-    var tintRGB = colorValue.slice(4, -1).split(", ");
-    var r = parseInt(tintRGB[0], 10);
-    var g = parseInt(tintRGB[1], 10);
-    var b = parseInt(tintRGB[2], 10);
-    var newRGB = [0, 0, 0];
-
-    step = step == 0 ? 0.3:step;
-
-    newRGB[0] = Math.floor(r / factor*step);
-    newRGB[1] = Math.floor(g / factor*step);
-    newRGB[2] = Math.floor(b / factor*step);
-
-    console.log(newRGB, factor);
-    return "rgb(" + newRGB[0] + ", " + newRGB[1] + ", " + newRGB[2] + ")";
-}
-
-function calcTint(colorValue, factor) {
-    var tintRGB = colorValue.slice(4, -1).split(", ");
-    var r = parseInt(tintRGB[0], 10);
-    var g = parseInt(tintRGB[1], 10);
-    var b = parseInt(tintRGB[2], 10);
-    var newRGB = [0, 0, 0];
-    newRGB[0] = Math.floor(((255 - r) * factor) / 10 + r);
-    newRGB[1] = Math.floor(((255 - g) * factor) / 10 + g);
-    newRGB[2] = Math.floor(((255 - b) * factor) / 10 + b);
-    return "rgb(" + newRGB[0] + ", " + newRGB[1] + ", " + newRGB[2] + ")";
-}
-
-function calcRelativeLuminance(colorValue) {
-    var colorRGB = colorValue.slice(4, -1).split(", ");
-    colorRGB[0] = normalizeValue(colorRGB[0]) * 0.2126;
-    colorRGB[1] = normalizeValue(colorRGB[1]) * 0.7152;
-    colorRGB[2] = normalizeValue(colorRGB[2]) * 0.0722;
-    var luminance = colorRGB[0] + colorRGB[1] + colorRGB[2];
-    return luminance;
-}
-
-function normalizeValue(singleColorValue) {
-    singleColorValue = singleColorValue / 255;
-    if (singleColorValue <= 0.03928) {
-        console.log("value is darker")
-        singleColorValue = 0.03928 / 12.92;
-    } else {
-        console.log("value is brighter")
-        singleColorValue = ((singleColorValue + 0.055) / 1.055) ** 2.4;
-    }
-    return singleColorValue;
+function addToDocumentColors(group,document) {
+    group.layers.map(layer => {
+        document.colors.push({
+            name: layer.name,
+            style: layer.style.fills[0].color
+        });
+    });
 }
