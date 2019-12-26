8321eee2d8d574bc0ca7b9ca49889c1a9b549bea
chewbecca <beckybettinger@yahoo.de>
2019-12-26 13:21:43 +0100

    fixed layer selection

--- src/my-command.js
+++ src/my-command.js
@@ -9,36 +9,35 @@ let selectedColor = {};
 export default function(context) {
   let selection = document.selectedLayers;
 
-  if (selection.isEmpty) {
+  if (
+    selection.isEmpty ||
+    selection.length > 1 ||
+    selection.layers[0].type === "Artboard"
+  ) {
     sketch.UI.alert(
-      "No Layer Selected",
+      "Layer with color fill needed",
       "Please select exactly one Layer with a color fill to caluclate tints and shades of this color"
     );
+  } else if (!selection.layers[0].style.fills[0].enabled) {
+    console.log("test");
+    sketch.UI.alert("Color fill is inactive", "Plaese select a shape layer with an active color fill.");
   } else {
-    if (selection.length > 1) {
-      sketch.UI.alert(
-        "More Then One Layer Selected",
-        "Please select exactly one Layer with a color fill to caluclate tints and shades of this color"
-      );
-    } else {
-      if (selection.layers[0].style){
-        selectedColor = colorCalc.sketchHexToColorObject(selection.layers[0].style.fills[0].color);
-        sketch.UI.getInputFromUser(
-            "Please specifiy the name of your Colors",
-            {
-              initialValue: "Primary Color"
-            },
-            (err, value) => {
-              if (err) {
-                // most likely the user canceled the input
-                return
-              }
-              if (value) {
-                createSketchAssets(selectedColor, document, value);
-              }
-            }
-        )
+    const fill = selection.layers[0].style.fills[0];
+    selectedColor = colorCalc.sketchHexToColorObject(fill.color);
+    sketch.UI.getInputFromUser(
+      "Please specifiy the name of your Colors",
+      {
+        initialValue: "Primary Color"
+      },
+      (err, value) => {
+        if (err) {
+          // most likely the user canceled the input
+          return;
+        }
+        if (value) {
+          createSketchAssets(selectedColor, document, value);
+        }
       }
-    }
+    );
   }
 }
